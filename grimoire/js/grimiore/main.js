cbc898239f2293677a87a223bf366c442b1bd608
Mo Silvera <mo.leslie.silvera@gmail.com>
2020-05-13 12:21:52 -0500

    rolling dice and grimoire complete

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,6 +1,51 @@
 function main() {
-  // Put your code here
-
+  console.log("Do you believe in magic?")
+  console.log("-------------------")
+  const goodBook = makeGoodSpellBook(allSpells)
+  const evilBook = makeEvilSpellBook(allSpells)
+  displaySpellBook(goodBook)
+  displaySpellBook(evilBook)
 }
+
+const displaySpellBook = (book) => {
+  console.log(book.title)
+  book.spells.forEach(spell => {
+    console.log(spell.name)
+  });
+}
+const makeEvilSpellBook=(spells) => {
+  let evilBook = {}
+  evilBook.title = "Evil Book"
+  evilBook.spells = spells.filter(spell => spell.isEvil === true)
+  return evilBook
+}
+const makeGoodSpellBook = (spells) => {
+  let goodBook = {}
+  goodBook.title = "Good Book"
+  goodBook.spells = spells.filter(spell => spell.isEvil === false)
+  return goodBook
+}
+const allSpells = [
+  {
+    name: "turn enemy into a newt",
+    isEvil: true,
+    energyRequired: 5.1
+  },
+  {
+    name: "turn enemy into a happy newt",
+    isEvil: false,
+    energyRequired: 2
+  },
+  {
+    name: "feed the poor",
+    isEvil: false,
+    energyRequired: 5.1
+  },
+  {
+    name: "make yourself emperor",
+    isEvil: true,
+    energyRequired: 5.1
+  },
+]
 
 main();
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,6 +1,50 @@
-function main() {
-  // Put your code here
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+
+ const toString = (value) => {
+    let dieString = "Unknown";
+            switch (value)
+            {
+                case 1:
+                    dieString = "\u2680";
+                    break;
+                case 2:
+                    dieString = "\u2681";
+                    break;
+                case 3:
+                    dieString = "\u2682";
+                    break;
+                case 4:
+                    dieString = "\u2683";
+                    break;
+                case 5:
+                    dieString = "\u2684";
+                    break;
+                case 6:
+                    dieString = "\u2685";
+                    break;
+            }
+
+            return dieString;
+  }
+
+
+function main() {
+  console.log("Let's roll some dice, baby!")
+  console.log("----------------------------")
+  for (let i = 0; i < 10; i++) {
+    let die1 = getRandomInt(1,6)
+    let die2 = getRandomInt(1,6)
+    let message = `${toString(die1)} + ${toString(die2)} === ${die1 + die2} `
+    if (die1 === die2){
+      message += "DOUBLES!"
+    }
+    console.log(message)
+  }
 }
 
 main();
